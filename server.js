2b2c6ac503b7ef6905d45ff73d3c3c18a387b1db
Feraz ahmad <ferazahmadgit62@gmail.com>
2024-05-10 18:56:30 +0530

    port coming from enviroment

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const app = require("./app");
 
-const PORT = 3000;
 dotenv.config({ path: "./config.env" });
 
 const DB = process.env.DATABASE.replace(
@@ -16,6 +15,8 @@ mongoose
   .then((connection) => {})
 
   .catch((error) => console.log(error));
+
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log("app is  runniong");
 });
